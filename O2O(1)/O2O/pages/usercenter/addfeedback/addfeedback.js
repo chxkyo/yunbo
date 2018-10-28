991ce97483fdc62c15c3001e232fc842a61d0aa7
kyo <2773052294@qq.com>
2018-10-28 22:08:05 +0800

    2018/10/28 22:07

--- O2O(1)/O2O/pages/usercenter/addfeedback/addfeedback.js
+++ O2O(1)/O2O/pages/usercenter/addfeedback/addfeedback.js
@@ -3,17 +3,21 @@ const app = getApp()
 Page({
   data: {
     addFeedBackSuccShow: false, //提交成功弹窗是否显示,
-    content:''
+    content:'',
+    length:100
   },
   onLoad: function (options) {
 
   },
   getFeedbackContent(e){
     this.setData({
-      content:e.detail.value
+      content:e.detail.value,
+      length: 100 - e.detail.value.length
     })
   },
   submit(){
+    let pages = getCurrentPages();
+    let prevPage = pages[pages.length - 2];  //上一个页面
     wx.showLoading({
       title: '提交反馈中....',
     })
@@ -22,6 +26,15 @@ Page({
       if (res.data.success) {
         this.setData({
           addFeedBackSuccShow: true
+        })
+        wx.showToast({
+          title: '提交反馈成功！',
+          success: function () {
+            prevPage.onLoad();
+            wx.navigateBack({
+              delta: 1
+            })
+          }
         })
       }
     });
--- O2O(1)/O2O/pages/usercenter/myprofile/myprofile.js
+++ O2O(1)/O2O/pages/usercenter/myprofile/myprofile.js
@@ -2,12 +2,14 @@
 const app = getApp()
 Page({
   data: {
-    sexIndex: 1,
+    sexIndex: 0,
     sexArray: ['女', '男'],
+    constellationIndex:0,
+    constellationArray: ['白羊座','金牛座','双子座','巨蟹座','狮子座','处女座','天秤座','天蝎座','射手座','摩羯座','水瓶座','双鱼座'],
     region: ['','',''],
     name:'',
     sex:'',
-    birthday:'',
+    birthday:'1971-09-14',
     sign:'',
     mobile:'',
     email:'',
@@ -31,15 +33,22 @@ Page({
         }else{
           this.data.region[1] = res.data.data.user.cityName;
         }
+        let index = this.data.constellationArray.findIndex(function(val,index){
+          return val == res.data.data.user.sign;
+        })
+        if(index == -1){
+          index = 0;
+        }
+        console.log(index)
         this.setData({
           name:res.data.data.user.name,
           sexIndex: res.data.data.user.sex,
           birthday: res.data.data.user.birthday,
-          sign: res.data.data.user.sign,
           mobile: res.data.data.user.mobile,
           email: res.data.data.user.email,
           addrDetail: res.data.data.user.addrDetail,
-          region: this.data.region
+          region: this.data.region,
+          constellationIndex: index
         });
       }
     });
@@ -66,7 +75,7 @@ Page({
   },
   getSign(e){
     this.setData({
-      sign: e.detail.value
+      constellationIndex: e.detail.value
     })
   },
   getMobile(e){
@@ -90,7 +99,7 @@ Page({
       mask:true
     })
     this.userId = "628800148082";
-    app.fetch("snail-portal/user/updateUserInfoAjax.do?userId=" + this.data.userId + "&name=" + this.data.name + "&sex=" + this.data.sexIndex + "&sign=" + this.data.sign + "&mobile=" + this.data.mobile + "&email=" + this.data.email + "&addrDetail=" + this.data.addrDetail + "&provinceName=" + this.data.region[0] + "&cityName=" + this.data.region[1]).then(res => {
+    app.fetch("snail-portal/user/updateUserInfoAjax.do?userId=" + this.data.userId + "&name=" + this.data.name + "&sex=" + this.data.sexIndex + "&sign=" + this.data.constellationArray[this.data.constellationIndex] + "&mobile=" + this.data.mobile + "&email=" + this.data.email + "&addrDetail=" + this.data.addrDetail + "&provinceName=" + this.data.region[0] + "&cityName=" + this.data.region[1] + "&birthday="+this.data.birthday).then(res => {
       wx.hideLoading();
       if (res.data.success) {
         wx.showToast({
